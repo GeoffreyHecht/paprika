b66e37eeea404f66d01dfee5fc72c3c0cbea7eac
GeoffreyHecht <geoffrey.hecht@gmail.com>
2014-06-24 13:53:17 +0200

    CBO added

---
+++ src/paprika/metrics/CouplingBetweenObjects.java
@@ -0,0 +1,21 @@
+package paprika.metrics;
+
+import paprika.entities.PaprikaClass;
+
+/**
+ * Created by Geoffrey Hecht on 20/05/14.
+ */
+public class CouplingBetweenObjects extends UnaryMetric<Integer> {
+
+    private CouplingBetweenObjects(PaprikaClass paprikaClass) {
+        this.value = paprikaClass.getCouplingValue();
+        this.entity = paprikaClass;
+        this.name = "Coupling Between Object Classes";
+    }
+
+    public static CouplingBetweenObjects createCouplingBetweenObjects(PaprikaClass paprikaClass) {
+        CouplingBetweenObjects couplingBetweenObjects = new CouplingBetweenObjects(paprikaClass);
+        couplingBetweenObjects.updateEntity();
+        return couplingBetweenObjects;
+    }
+}
