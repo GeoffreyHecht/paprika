b99118ae017f1e67de3f4a91d31bbf4f2961e817
GeoffreyHecht <geoffrey.hecht@gmail.com>
2014-07-07 10:10:10 +0200

    LCOM added

---
+++ src/paprika/metrics/LackofCohesionInMethods.java
@@ -0,0 +1,21 @@
+package paprika.metrics;
+
+import paprika.entities.PaprikaClass;
+
+/**
+ * Created by Geoffrey Hecht on 20/05/14.
+ */
+public class LackofCohesionInMethods extends UnaryMetric<Integer> {
+
+    private LackofCohesionInMethods(PaprikaClass paprikaClass) {
+        this.value = paprikaClass.computeLCOM();
+        this.entity = paprikaClass;
+        this.name = "Lack of Cohesion in Methods";
+    }
+
+    public static LackofCohesionInMethods createLackofCohesionInMethods(PaprikaClass paprikaClass) {
+        LackofCohesionInMethods couplingBetweenObjects = new LackofCohesionInMethods(paprikaClass);
+        couplingBetweenObjects.updateEntity();
+        return couplingBetweenObjects;
+    }
+}
