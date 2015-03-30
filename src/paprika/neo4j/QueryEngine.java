e654b70bf1f3886ab5e61c79cad2c69d3e2de096
GeoffreyHecht <geoffrey.hecht@gmail.com>
2015-03-30 18:32:44 +0200

    stats mode

--- src/paprika/neo4j/QueryEngine.java
+++ src/paprika/neo4j/QueryEngine.java
@@ -7,6 +7,7 @@ import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Transaction;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Geoffrey Hecht on 12/01/15.
@@ -24,6 +25,10 @@ public class QueryEngine {
         engine = new ExecutionEngine(graphDatabaseService);
     }
 
+    public void shutDown(){
+        databaseManager.shutDown();
+    }
+
    public void MIMQuery() throws CypherException {
        ExecutionResult result;
        try (Transaction ignored = graphDatabaseService.beginTx()) {
@@ -37,5 +42,47 @@ public class QueryEngine {
            System.out.println(result.dumpToString());
        }
    }
+
+
+    public double calculateHighThreshold(String nodeType, String property){
+        double res = 0;
+        ExecutionResult result;
+        try (Transaction ignored = graphDatabaseService.beginTx()) {
+            String query = "MATCH (n:"+nodeType+") RETURN percentileDisc(n."+property+",0.25) as Q1, percentileDisc(n."+property+",0.75) as Q3";
+            result = engine.execute(query);
+            //Only one result in that case
+            for ( Map<String, Object> row : result )
+            {
+                int q1 = (int) row.get("Q1");
+                int q3 = (int) row.get("Q3");
+                res = q3 + ( 1.5 * ( q3 - q1));
+            }
+        }
+        return res;
+    }
+
+    public double calculateClassComplexityThreshold(){
+        return calculateHighThreshold("Class", "class_complexity");
+    }
+
+    public double calculateNumberofInstructionsThreshold(){
+        return calculateHighThreshold("Method","number_of_instructions");
+    }
+
+    public double calculateNumberOfImplementedInterfacesThreshold(){
+        return calculateHighThreshold("Class","number_of_implemented_interfaces");
+    }
+
+    public double calculateLackofCohesionInMethodsThreshold(){
+        return calculateHighThreshold("Class","lack_of_cohesion_in_methods");
+    }
+
+    public double calculateNumberOfMethodsThreshold(){
+        return calculateHighThreshold("Class","number_of_methods");
+    }
+
+    public double calculateNumberOfAttributesThreshold(){
+        return calculateHighThreshold("Class","number_of_attributes");
+    }
 
 }
