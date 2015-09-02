72c38ad5e449a49aae854a33643c37180ce299eb
GeoffreyHecht <geoffrey.hecht@gmail.com>
2015-09-02 13:29:39 -0400

    Fix SAK/IGS file name

--- src/paprika/neo4j/IGSQuery.java
+++ src/paprika/neo4j/IGSQuery.java
@@ -24,7 +24,7 @@ public class IGSQuery extends Query {
         try (Transaction ignored = graphDatabaseService.beginTx()) {
             String query = "MATCH (a:App) WITH a.app_key as key MATCH (cl:Class {app_key: key})-[:CLASS_OWNS_METHOD]->(m1:Method {app_key: key})-[:CALLS]->(m2:Method {app_key: key}) WHERE (m2.is_setter OR m2.is_getter) AND cl-[:CLASS_OWNS_METHOD]->m2 RETURN m1.app_key as app_key,count(m1) as IGS";
             Result result = graphDatabaseService.execute(query);
-            queryEngine.resultToCSV(result, "_IGS_NO_FUZZY.csv");
+            queryEngine.resultToCSV(result, "_IGS.csv");
         }
     }
 }
