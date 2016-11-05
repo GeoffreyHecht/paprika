e5ab0ac64f01e1011abc4f0082f00506784bef7f
GeoffreyHecht <geoffrey.hecht@gmail.com>
2016-11-05 12:53:54 +0100

    Custom request now give results in a CSV

--- src/paprika/neo4j/QueryEngine.java
+++ src/paprika/neo4j/QueryEngine.java
@@ -1,3 +1,21 @@
+/*
+ * Paprika - Detection of code smells in Android application
+ *     Copyright (C)  2016  Geoffrey Hecht - INRIA - UQAM - University of Lille
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as
+ *     published by the Free Software Foundation, either version 3 of the
+ *     License, or (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Affero General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Affero General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package paprika.neo4j;
 
 import org.neo4j.cypher.CypherException;
@@ -321,9 +339,11 @@ public class QueryEngine {
         }
     }
 
-    public void executeRequest(String request){
+    public void executeRequest(String request)  throws CypherException, IOException {
+        Result result;
         try (Transaction ignored = graphDatabaseService.beginTx()) {
-            graphDatabaseService.execute(request);
+            result = graphDatabaseService.execute(request);
+            resultToCSV(result,"_CUSTOM.csv");
         }
     }
 }
